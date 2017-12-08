81ddeafaf81c192ec60603c3df7a9639abef87df
Nico Otremba <nico.otremba@mmi-media.de>
2017-12-08 22:19:11 +0100

    added c# project

---
+++ Eel Speel/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float playerSpeed;
+
+	private Rigidbody playerRigid;
+
+	void Start()
+	{
+		playerRigid = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		float speed = playerSpeed;
+		if (lookingAtBuff)
+		{
+			yield return new waitForSeconds(0.1);
+			speed *= speedBuff;
+		}
+		#region player Movement
+		Vector3 force = new Vector3();
+		#endregion
+	}
+}
