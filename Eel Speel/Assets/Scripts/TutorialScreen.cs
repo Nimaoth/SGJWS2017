4289e045b550e392d593a196f6ab4e5e5284c8bd
LAPTOP-NNVPUIJK\viola <viola.stumpf@mmi-media.de>
2017-12-10 04:43:55 +0100

    added tutorial screen

---
+++ Eel Speel/Assets/Scripts/TutorialScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown("joystick 1 button 2") || Input.GetKeyDown("joystick 2 button 2") || Input.GetKeyDown("joystick 3 button 2") || Input.GetKeyDown("joystick 4 button 2"))
+        {
+            SceneManager.LoadScene("menu");
+        }
+	}
+}
