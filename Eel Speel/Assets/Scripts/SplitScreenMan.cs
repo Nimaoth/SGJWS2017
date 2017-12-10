cd928410078566faf59c19b05a2e90dbf98b7a7a
Nico Otremba <nico.otremba@mmi-media.de>
2017-12-10 03:49:57 +0100

    changed ui

--- Eel Speel/Assets/Scripts/SplitScreenMan.cs
+++ Eel Speel/Assets/Scripts/SplitScreenMan.cs
@@ -16,7 +16,7 @@ public class SplitScreenMan : MonoBehaviour {
 			{
 				p.SetActive(true);
 				players.Add(p.GetComponentInChildren<FollowPath>());
-				players[i].totalDistance = 4 - i;
+				players[i].totalDistance = 0.1f * (4 - i);
 			}
 		}
 		
