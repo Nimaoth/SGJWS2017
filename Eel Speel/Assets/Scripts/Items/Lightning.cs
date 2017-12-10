79ad8b2bc9215314df0d6fd9e8f7e5082ca329ff
Nico Otremba <nico.otremba@mmi-media.de>
2017-12-10 06:53:06 +0100

    added lightning

---
+++ Eel Speel/Assets/Scripts/Items/Lightning.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Linq;
+
+public class Lightning : MonoBehaviour {
+
+	public LineRenderer line;
+
+	public float speed;
+	public float range;
+	public float lifetime;
+	public int max;
+
+	void Start() {
+		StartCoroutine(uiae());
+	}
+
+	private IEnumerator uiae()
+	{
+		var start = Time.time;
+		while (start + lifetime > Time.time)
+		{
+			var arr = new Vector3[line.positionCount + 1];
+			line.GetPositions(arr);
+
+			int maxSkip = Mathf.Max(arr.Length - max, 0);
+			arr = arr.Skip(maxSkip).ToArray();
+			arr[arr.Length - 1] = new Vector3(Random.Range(-range, range),
+				Random.Range(-range, range),
+				arr[arr.Length - 2].z + speed * Time.deltaTime);
+
+			line.positionCount = arr.Length;
+			line.SetPositions(arr);
+
+			yield return null;
+		}
+
+		Destroy(gameObject);
+	}
+}
---
+++ Eel Speel/Assets/Scripts/Items/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public GameObject lightningPrefab;
+	public int count;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < count; i++)
+		{
+			Instantiate(lightningPrefab, Vector3.zero, Quaternion.identity, transform);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
